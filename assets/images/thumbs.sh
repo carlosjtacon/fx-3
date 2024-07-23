baa88d96ab28b0c60964ff31cd4c9e57c2101804
Carlos Tacón <carlosjtacon@gmail.com>
2024-07-23 19:58:39 +0200

    thumbnail generator

---
+++ assets/images/thumbs.sh
@@ -0,0 +1,11 @@
+#! /bin/bash 
+# requires imagemagick installed - brew install imagemagick
+THUMB_MAX=1024
+for i in *.jpeg
+do
+    if [[ $i != *-thumb.webp ]]
+    then
+        filename=`basename -s .jpeg $i`
+        magick "$i" -thumbnail "${THUMB_MAX}x${THUMB_MAX}>" "$filename-thumb.webp"
+    fi
+done
